fd12fdbd0b438086ba61e2bd4e824d14b5d955f3
DMon <davemonaghan@gmx.com>
2022-09-02 20:05:21 +0100

    license badge functions working

--- Develop/generateMarkdown.js
+++ Develop/generateMarkdown.js
@@ -1,19 +1,88 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const fs = require('fs')
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseBadge(license) {
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-`;
+  if (license === "MIT") {
+    return  '![License](https://img.shields.io/badge/license-MIT-blue)](https://opensource.org/licenses/MIT)'
+  }
+  if (license === "APACHE 2.0") {
+    return'![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  }
+  else if (license === 'GPL 3.0') {
+    return '![License](https://img.shields.io/badge/License-GNU%20GPL-blue)](https://opensource.org/licenses/gpl-license)'
+
+  }
+  else (license === 'None')
+  return ""
+
 }
 
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return `\n* [License](#License)`
+  } return ""
+
+
+}
+
+
+function renderLicenseSection(license) { 
+  if (license !== "None") {
+    return `\n# License:
+  
+    This project is licensed under:
+    
+    ${license}`
+  } return ""
+}
+
+
+function generateMarkdown(response) {
+  
+  return `# ${response.title}        
+  
+  ${renderLicenseBadge(response.license)}
+  
+  ${response.description}
+  
+  ## Table of Contents:
+  
+  * [Installation](#Installation)
+
+  * [Usage](#Usage)
+  ${renderLicenseLink(response.license)}
+  * [Contributing](#Contributing)
+
+  * [Tests](#Tests)
+
+  * [Questions](#Questions)
+  
+  ### Installation:
+  
+  ${response.installation}
+  
+  # Usage:
+  
+  ${response.usage}
+  ${renderLicenseSection(response.license)}
+  # Contributing:
+  
+  ${response.contributing}
+  
+  # Tests:
+  
+  ${response.tests}
+  
+  # Questions:
+  
+  For any additional Information. Please contact me on:
+  ${response.github}
+  
+  or by email - ${response.email}`;
+
+  };
+
+
 module.exports = generateMarkdown;
+
--- Develop/index.js
+++ Develop/index.js
@@ -1,117 +1,67 @@
+const generateMarkdown = require("./generateMarkdown")
 const inquirer = require('inquirer');
 const fs = require('fs')
 
+const questions = [
+  {
+    type: 'input',
+    message: 'What is the name of your project?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'Please input a description of your project?',
+    name: 'description',
+  },
 
-const questions = () =>
+  {
+    type: 'input',
+    message: 'Please enter any installation instructions?',
+    name: 'installation ',
+  },
+  {
+    type: 'input',
+    message: 'Please enter usage instructions?',
+    name: 'usage',
+  },
+  {
+    type: 'input',
+    message: 'Please enter details of any contributors?',
+    name: 'contributing',
+  },
+  {
+    type: 'list',
+    message: 'What kind of license does your project have?',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'None'],
+    name: 'license',
+  },
+  {
+    type: 'input',
+    message: 'Please enter any instructions required to run tests?',
+    name: 'tests',
+  },
+  {
+    type: 'input',
+    message: 'Please enter your github user name?',
+    name: 'github',
+  },
+  {
+    type: 'input',
+    message: 'Please enter your email address?',
+    name: 'email',
+  },
+];
+
 
 inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'What is the name of your project?',
-      name: 'title',
-    },
-    {
-      type: 'input',
-      message: 'Please input a description of your project?',
-      name: 'description',
-    },
-
-    {
-      type: 'input',
-      message: 'Please enter any installation instructions?',
-      name: 'installation ',
-    },
-    {
-      type: 'input',
-      message: 'Please enter usage instructions?',
-      name: 'usage',
-    },
-    {
-      type: 'input',
-      message: 'Please enter details of any contributors?',
-      name: 'contributing',
-    },
-    {
-      type: 'list',
-      message: 'What kind of license does your project have?',
-      choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
-      name: 'license',
-    },
-    {
-      type: 'input',
-      message: 'Please enter any instructions required to run tests?',
-      name: 'tests',
-    },
-    {
-      type: 'input',
-      message: 'Please enter your github user name?',
-      name: 'github',
-    },
-    {
-      type: 'input',
-      message: 'Please enter your email address?',
-      name: 'email',
-    },
-  ])
-
+  .prompt(questions)
   .then((response) => {
-    console.log(response)
-
-    const generateReadMe = (response) =>
-
-`# ${response.title} 
-
-
-${response.description}
-
-## Table of Contents:
-
-* [Installation](#Installation)
-* [Usage](#Usage)
-* [License](#License)
-* [Contributing](#Contributing)
-* [Tests](#Tests)
-* [Questions](#Questions)
-
-### Installation:
-
-${response.instalation}
-
-# Usage:
-
-${response.usage}
-
-# License:
-
-This project is licensed under:
-
-${response.license}
-
-# Contributing:
-
-${response.contributing}
-
-# Tests:
-
-${response.tests}
-
-# Questions:
-
-For any additional Information. Please contact me on:
-${response.github}
-
-or by email - ${response.email}`;
-
-
-    const readMePage = generateReadMe(response);
-    fs.writeFile('readme.md', readMePage, (err) =>
+    fs.writeFile('readme.md', generateMarkdown(response), (err) =>
       err ? console.log(err) : console.log('Successfully created readme.md!')
 
     );
-  });
+  })
 
 
 
-  questions ()
 
